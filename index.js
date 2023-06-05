dc1f2a8453f269ede15989e9e257bc3fac81be3f
Murthy Karra <karrad@karra.co>
2023-06-05 01:48:32 -0700

    Added debounce timer

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'b6452fc2',
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const debounce = (func) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, 1000);
+  };
+};
+
+// let timeoutId;
+// const onInput = (event) => {
+//   if (timeoutId) {
+//     clearTimeout(timeoutId);
+//   }
+//   timeoutId = setTimeout(() => {
+//     fetchData(event.target.value);
+//   }, 1000);
+// };
+
+const onInput = debounce((event) => {
+  fetchData(event.target.value);
+});
+
+input.addEventListener('input', onInput);
